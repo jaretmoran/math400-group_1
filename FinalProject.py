10eb98bed411e45f368adcac54f084fd42f58e5d
jaretmoran <116687940+jaretmoran@users.noreply.github.com>
2022-11-14 17:08:44 -0800

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -14,6 +14,7 @@ import secant
 import newton
 import center_difference as cd
 import gaussian_elimination_w_pp as pp
+from scipy.integrate import quad
 
 def newtonMethod(f, x0, delta, maxIterations):
     precision = "0.6E"    
@@ -200,6 +201,21 @@ def pm2(x0, h=0.0125):
     print("gx = ", g(x[0], x[1], x[2]))
     print("mx = ", m(x[0], x[1], x[2]))
     
+def simpson(f, a, b, n):
+        h = float(b-a)/n
+        k = 0.0
+        x = a + h
+        for i in np.arange(1, n/2 + 1):
+            k += 4*f(x)
+            x += 2*h
+
+        x = a + 2*h
+        for i in np.arange(1, n/2):
+            k += 2*f(x)
+            x += 2*h
+        return (h/3)*(f(a)+f(b)+k)
+
+    
 def main():
     
     ########################################################################
@@ -239,6 +255,62 @@ def main():
     x0 = np.array([2, 0, 2])
     pm2(x0)
     '''
+    
+    #######################################################################
+    ####### Problem 3
+    #######################################################################
+    #first test case
+    '''
+    f = lambda x: x**3+x**2+x
+    F = lambda x: x**3+x**2+x
+
+    #second test case
+    f1 = lambda x: x**3
+    F1 = lambda x: x**3
+
+    #third test case
+    f2 = lambda x: 2*x**3+10*x**2
+    F2 = lambda x: 2*x**3+10*x**2
+
+    #intervals
+    a, b = 0.0, 2
+
+    #exact solutions for each equation 
+    exact = quad(F, a, b)
+
+    exact1 = quad(F1, a, b)
+
+    exact2 = quad(F2, a, b)
+
+
+    #printing exact value, simpsons approximation and error
+    nvalues = [2**i for i in range(1,5)]
+    for n in nvalues:
+        errsimp = abs(exact[0] - simpson(f,a,b,n)) 
+        print(f"# of intervals={n}, Exact value: {exact[0]}, Simpson's approximation = {simpson(f,a,b,n)}, Simpson's error  = {errsimp}")
+
+    print("\n")
+
+
+    for n in nvalues:
+        errsimp = abs(exact[0] - simpson(f1,a,b,n))
+        print(f"# of intervals={n}, Exact value: {exact1[0]}, Simpson's approximation = {simpson(f1,a,b,n)}, Simpson's error  = {errsimp}")
+
+    print("\n")
+
+    for n in nvalues:
+        errsimp = abs(exact2[0] - simpson(f2,a,b,n))
+        print(f"# of intervals={n}, Exact value: {exact2[0]}, Simpson's approximation = {simpson(f2,a,b,n)}, Simpson's error  = {errsimp}")
+
+    print("\n")
+    '''
+    #######################################################################
+    ####### Problem 4
+    #######################################################################
+    
+
+    
+    
     
     return 0
 
